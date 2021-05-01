7f5ba57bb315fb9b0579ba37cc9319f360cfc15c
vishal pandey <vishalp.hestabit@gmail.com>
2021-05-01 13:45:11 +0530

    First commit

---
+++ database/migrations/2021_04_30_215714_alter_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('army_no');
+            $table->bigInteger('trade_id');
+            $table->bigInteger('clo_card_no');
+            $table->bigInteger('rank_id');
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
